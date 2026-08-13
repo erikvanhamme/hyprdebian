7f97217bdf508cb3fbc098914b273542f26af285
Erik Van Hamme <erik.vanhamme@gmail.com>
2026-08-13 10:58:38 +0200

    Adds proper shutdown and reboot scripts.

--- scripts/t_base.sh
+++ scripts/t_base.sh
@@ -130,7 +130,7 @@ base_utilities() {
     # Note: command-not-found requires an update to the apt-file cache to work.
     in_target update-command-not-found
 
-    add_packages eza yazi fzf nfs-common psmisc net-tools pciutils usbutils acpi bash-completion git-delta
+    add_packages eza yazi fzf nfs-common psmisc net-tools pciutils usbutils acpi bash-completion git-delta ack
 }
 
 tgt_netplan() {
