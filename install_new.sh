9138b2aff1cd080eb65d4152ba84f069d4739f1c
Erik Van Hamme <erik.vanhamme@gmail.com>
2026-09-03 23:03:32 +0200

    Bugfixing.

--- install_new.sh
+++ install_new.sh
@@ -9,7 +9,7 @@ source scripts/dependencies.sh
 source scripts/helpers.sh
 
 # Start banner.
-echo "=== Erik's nifty debian+hyprland installer v0.67 ==="
+echo "=== Erik's nifty debian+hyprland installer v0.68 ==="
 
 # Set up idempotency and config paths.
 mkdir -p "$STATE_DIR"
